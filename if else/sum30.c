6f2c7148193e1e1d171b60f4c221dc6713584f91
dishalukhi123 <150135853+dishalukhi123@users.noreply.github.com>
2025-05-02 14:07:15 +0530

    loop 9.1

--- if else/sum30.c
+++ if else/sum30.c
@@ -1,25 +1,28 @@
 #include <stdio.h>
 
-int main() {
-    int choice;
-    int a, b, c, sum;
+int main()
+{
+    int a, b, c, sum, choice;
 
     printf("Enter 2 to add two numbers or 3 to add three numbers: ");
     scanf("%d", &choice);
 
-    if (choice == 2) {
+    if (choice == 2)
+    {
         printf("Enter two numbers: ");
         scanf("%d %d", &a, &b);
         sum = a + b;
         printf("Sum = %d\n", sum);
     }
-    else if (choice == 3) {
+    else if (choice == 3)
+    {
         printf("Enter three numbers: ");
         scanf("%d %d %d", &a, &b, &c);
         sum = a + b + c;
         printf("Sum = %d\n", sum);
     }
-    else {
+    else
+    {
         printf("Invalid input. Please enter 2 or 3.\n");
     }
 
