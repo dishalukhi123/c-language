9b57593e388b78851cac9fbc5733a32148d5eb3a
dishalukhi123 <150135853+dishalukhi123@users.noreply.github.com>
2025-05-03 10:17:59 +0530

    update PR 2

--- PR. 2 Simple Grade Calculator/gradecalculator.c
+++ PR. 2 Simple Grade Calculator/gradecalculator.c
@@ -6,26 +6,31 @@ int main()
     printf("Enter your score :-");
     scanf("%d", &val);
 
-    if (val >= 91 && val <= 100)
-    {
-        printf("your gread is A. Excellent work!");
-    }
-    else if (val >= 81 && val <= 90)
-    {
-        printf("your gread is B. Excellent work! you are eligible for the next level.");
-    }
-    else if (val >= 61 && val <= 80)
-    {
-        printf("your gread is C. Excellent work!");
-    }
-    else if (val >= 41 && val <= 60)
-    {
-        printf("your gread is D. Excellent work!");
-    }
-    else
-    {
-        printf("fail");
-    }
+    (val >= 91 && val <= 100) ? printf("Your grade is A. Excellent work!\n") : (val >= 81 && val <= 90) ? printf("Your grade is B. Excellent work! You are eligible for the next level.\n")
+                                                                           : (val >= 61 && val <= 80)   ? printf("Your grade is C. Excellent work!\n")
+                                                                           : (val >= 41 && val <= 60)   ? printf("Your grade is D. Excellent work!\n")
+                                                                                                        : printf("Fail\n");
+
+    // if (val >= 91 && val <= 100)
+    // {
+    //     printf("your gread is A. Excellent work!");
+    // }
+    // else if (val >= 81 && val <= 90)
+    // {
+    //     printf("your gread is B. Excellent work! you are eligible for the next level.");
+    // }
+    // else if (val >= 61 && val <= 80)
+    // {
+    //     printf("your gread is C. Excellent work!");
+    // }
+    // else if (val >= 41 && val <= 60)
+    // {
+    //     printf("your gread is D. Excellent work!");
+    // }
+    // else
+    // {
+    //     printf("fail");
+    // }
 
     return 0;
 }
